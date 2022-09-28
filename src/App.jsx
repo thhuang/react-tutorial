646aace1695fde43b3fcc816e6603cb52a05a066
TH Huang <tzhsn.huang@gmail.com>
2022-09-27 21:23:39 -0500

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { useJsonQuery } from './utilities/fetch';
 import Banner from './components/Banner.jsx';
 import CourseList from './components/courselist/CourseList.jsx';
 
-const schedule = {
-  title: 'CS Courses for 2018-2019',
-  courses: {
-    F101: {
-      term: 'Fall',
-      number: '101',
-      meets: 'MWF 11:00-11:50',
-      title: 'Computer Science: Concepts, Philosophy, and Connections',
-    },
-    F110: {
-      term: 'Fall',
-      number: '110',
-      meets: 'MWF 10:00-10:50',
-      title: 'Intro Programming for non-majors',
-    },
-    S313: {
-      term: 'Spring',
-      number: '313',
-      meets: 'TuTh 15:30-16:50',
-      title: 'Tangible Interaction Design and Learning',
-    },
-    S314: {
-      term: 'Spring',
-      number: '314',
-      meets: 'TuTh 9:30-10:50',
-      title: 'Tech & Human Interaction',
-    },
-  },
-};
+const queryClient = new QueryClient();
 
 const App = () => (
-  <div className="container">
-    <Banner title={schedule.title}></Banner>
-    <CourseList courses={schedule.courses}></CourseList>
-  </div>
+  <QueryClientProvider client={queryClient}>
+    <Landing />
+  </QueryClientProvider>
 );
+
+const Landing = () => {
+  const [data, isLoading, error] = useJsonQuery(
+    'https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php'
+  );
+
+  if (error) return <div>Error: {error}</div>;
+  if (isLoading) return <div>Loading ...</div>;
+
+  return (
+    <div className="container">
+      <Banner title={data.title}></Banner>
+      <CourseList courses={data.courses}></CourseList>
+    </div>
+  );
+};
 
 export default App;
